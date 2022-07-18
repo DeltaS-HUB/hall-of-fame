ea9ab1d14519096a3ceee0f6d512ab46183dc173
Tobias.R <tobixir@gmail.com>
2022-07-18 11:24:18 -0300

    Barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar/NavVar';
 
 function App() {
+  const styleApp = { padding: '10px 20px', maginTop: 10}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //JSX
+    <div className="App" Style={styleApp}>
+      <h1>Hall of fmae</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavVar.js
@@ -0,0 +1,18 @@
+// UpperCamelCase
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1>Logo</h1>
+            <ul>
+                <li><button>Inicio</button></li>
+                <li><button>Ofertas</button></li>
+                <li><button>Juegos de cartas</button></li>
+                <li><button>Juegos de mesa</button></li>
+                <li><button>Taller de figuras</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
